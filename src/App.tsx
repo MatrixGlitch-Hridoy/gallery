87bd2cce547b5cb0de4cc081436bc7f5391cd124
Rizvi Khalid Hridoy <rkhridoy68@gmail.com>
2023-08-14 16:55:59 +0600

    [roject initialized

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="container mt-5 border">
+      <div className="grid-wrap">
+        <div className="item ">1</div>
+        <div className="item item-2">2</div>
+        <div className="item item-3">3</div>
+        <div className="item item-4">4</div>
+        <div className="item item-5">5</div>
+        <div className="item item-6">6</div>
+        <div className="item item-7">7</div>
+        <div className="item item-8">8</div>
+        <div className="item item-9">9</div>
+        <div className="item item-10">10</div>
+        <div className="item item-11">11</div>
+        <div className="item item-12">12</div>
+        <div className="item item-13">13</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
